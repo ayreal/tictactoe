4c341e0fa7037efb953edf5202a2b5ba08b78129
Ariel Herman <aeh.herman@gmail.com>
2018-04-16 14:03:05 -0400

    re-organize file structure

---
+++ game_logic/gameboard.js
@@ -0,0 +1,9 @@
+// const gameBoard = (() => {
+//   const contents = [];
+//   const render = () => {};
+//   return { render };
+// })();
+
+const gameBoard = () => console.log("this is the gameBoard");
+
+module.exports.gameBoard = gameBoard;
---
+++ main.js
@@ -0,0 +1,4 @@
+// const gameBoard = require("./game_logic/gameBoard.js").gameBoard;
+// console.log(gameBoard);
+
+console.log("hi");
